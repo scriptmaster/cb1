f722f07cc469f0a48eeec555c7415ba64a884054
MSheriff <msheriffusa@gmail.com>
2020-04-26 21:59:18 +0530

    Only 4 .cs files

---
+++ Controllers/EvidencesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace covid_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EvidencesController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetCountries()
+        {
+            return new string[] { };
+        }
+
+        [HttpGet("{countryCode}")]
+        public ActionResult<string> GetByCountry(string countryCode)
+        {
+            return "CountryCode: " + countryCode;
+        }
+
+        [HttpPost]
+        public void HelpConfirmedCase(
+            [FromBody]
+            CovidCase c)
+        {
+            // 
+        }
+
+        // // PUT api/values/5
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody] string value)
+        // {
+        // }
+    }
+}
---
+++ Models/CovidCase.cs
@@ -0,0 +1,22 @@
+
+using System;
+
+public class CovidCase {
+    public string CaseStatus { get; set; }
+    public DateTime CaseDate { get; set; }
+    public char MaleFemale { get; set; }
+    public int Age { get; set; }
+    public string PatientNickName { get; set; }
+    public string PatientPreviousConditions { get; set; }
+    public string EvidenceSource { get; set; }
+    public string GoogleLocationUrl { get; set; }
+    public string EvidenceNickname1 { get; set; }
+    public string VolunteerNickname { get; set; }
+    public string VolunteerNotes { get; set; }
+    public string AdminNotes { get; set; }
+    public string Investigator1Nickname { get; set; }
+    public string Investigator2Nickname { get; set; }
+    public string Investigator1Notes { get; set; }
+    public string Investigator2Notes { get; set; }
+
+}
